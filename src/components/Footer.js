44ced70478b7ce33b62a9205011a49ba153e448f
rockyhappy <rachit22153019@akgec.ac.in>
2024-09-15 04:15:21 +0530

    Fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Rachit</h3>
+          <h3>Designed and Developed by Programming Club</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           {/* <h3>Copyright © {year} SB</h3> */}
